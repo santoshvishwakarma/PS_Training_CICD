1b49d437acf869afdaad9a693791aa3eab65c371
Santosh V <santosh.vishwakrma@gmail.com>
2026-08-25 21:35:45 +0530

    commented example tc

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,22 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.getByAltText('Playwright logo').click();
-  await page.getByLabel('Search').click();
-  await page.getByPlaceholder('Search').click();
-  await page.getByText('Playwright').click();
-  const href = await page.getAttribute('a', 'href');
-  await page.getByRole('link', { name: 'Playwright' }).click();
-  await expect(page).toHaveTitle(/Playwright/);
+  // await page.goto('https://playwright.dev/');
+  // await page.getByAltText('Playwright logo').click();
+  // await page.getByLabel('Search').click();
+  // await page.getByPlaceholder('Search').click();
+  // await page.getByText('Playwright').click();
+  // const href = await page.getAttribute('a', 'href');
+  // await page.getByRole('link', { name: 'Playwright' }).click();
+  // await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // // Click the get started link.
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
